75b3b0de871d7dce1e9c2bda5f2d10a7c589f722
kyfaiyya <dzakyfaiqfayyadhi@gmail.com>
2025-03-20 07:59:28 +0700

    p

--- gudang-alat-tulis/models/Item.js
+++ gudang-alat-tulis/models/Item.js
@@ -1,9 +1,18 @@
-import { Schema, model } from "mongoose";
+const mongoose = require("mongoose");
 
-const ItemSchema = new Schema({
-    name: { type: String, required: true },
-    quantity: { type: Number, required: true },
-    price: { type: Number, required: true }
+const ItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    }
 });
 
-export default model("Item", ItemSchema);
+module.exports = mongoose.model("Item", ItemSchema);
